411658e6d5716ab39bfddb5f6d063f1943c64002
椿山 <tsubakiyama@MBP-LATE2013-1.local>
2019-02-20 16:52:03 +0900

    共通する操作であるArrayConverterを書いた。

--- Command/ArrayConverter/ShiftFirstToLastConverter.php
+++ Command/ArrayConverter/ShiftFirstToLastConverter.php
@@ -6,17 +6,26 @@
  * Time: 14:28
  */
 
+//require_once '../../Board.php';
 require_once 'ArrayConverterInterface.php';
 
 class ShiftFirstToLastConverter implements ArrayConverterInterface
 {
+    /**
+     * 引数で渡された配列の最初の要素を取り出して最後に付け足した配列を返す
+     *
+     * @param array $array
+     * @return array
+     */
     public function execute(array $array): array
     {
+        $max = count($array);
         $dstArr = [];
 
-        $dstArr[0] = $array[1];
-        $dstArr[1] = $array[2];
-        $dstArr[2] = $array[0];
+        for($i = 0; $i < $max - 1; $i++) {
+            $dstArr[] = $array[$i + 1];
+        }
+        $dstArr[] = $array[0];
 
         return $dstArr;
     }
--- Command/ArrayConverter/ShiftLastToFirstConverter.php
+++ Command/ArrayConverter/ShiftLastToFirstConverter.php
@@ -10,16 +10,21 @@ require_once 'ArrayConverterInterface.php';
 
 class ShiftLastToFirstConverter implements ArrayConverterInterface
 {
+    /**
+     * 引数の配列の最後の要素を取り出して最初に移動させた配列を返す
+     */
     public function execute(array $array): array
     {
+        $max = count($array);
+
         $dstArr = [];
 
-        $dstArr[0] = $array[2];
-        $dstArr[1] = $array[0];
-        $dstArr[2] = $array[1];
+        $dstArr[0] = $array[$max - 1];
 
+        for($i = 1; $i < $max; $i ++) {
+            $dstArr[$i] = $array[$i - 1];
+        }
 
         return $dstArr;
     }
-
 }
